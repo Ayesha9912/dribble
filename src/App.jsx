a86adcb171c94eadcf933f64ac450cc0e814a574
mahdianaiebi@gmail.com <mahdianaiebi@gmail.com>
2024-11-06 18:39:18 +0500

    footer has been updated

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Navbar from './components/Navbar'
 import Section2 from './components/Section2'
 import Section1 from "./components/Section1";
+import Footer from './components/Footer';
 
 function App(){
   return(
@@ -9,7 +10,7 @@ function App(){
       <Navbar></Navbar>
       <Section1></Section1>
       <Section2></Section2>
-
+      <Footer></Footer>
     </div>
   )
 }
